e93ccd478e1a3854a56802f7aa236fa4b881744a
mahdi9925 <mahdimomeni178@gmail.com>
2021-08-04 22:54:41 +0430

    pushbutton add connect to Enter_url_window class

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
+
 from YouTube_dl import Ui_MainWindow
 from DialogEnterURL import Ui_Dialog
 from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
 import sys
 
 
-
 class Persenolize(QMainWindow, Ui_MainWindow):
     def __init__(self):
-        # super().__init__()
         super(Persenolize, self).__init__()
         self.initUI()
 
     def initUI(self):
         self.setupUi(self)
-        self.add.clicked.connect(self.enter)
         self.show()
 
+        self.add.clicked.connect(self.add_url)
+    
+    def add_url(self):
+        self.enter= Enter_url_window()
+        self.enter.show()
 
-    def enter(self):
-        Enter_url()
-class Enter_url(QDialog):
+
+class Enter_url_window(QDialog):
     def __init__(self):
         QDialog.__init__(self)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
 
-        self.show()
+        
+
 
 app = QApplication(sys.argv)
 main = Persenolize()
