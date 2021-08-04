323969c39ead1615c287f550625b4c0e2f35f9d8
mahdi9925 <mahdimomeni178@gmail.com>
2021-08-04 22:53:22 +0430

    add `EnterURL.py` and delete `DialogEnterURL.py`

--- EnterURL.py
+++ EnterURL.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'DialogEnterURL.ui'
+# Form implementation generated from reading ui file 'EnterURL.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
